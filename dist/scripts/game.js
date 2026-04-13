e046623bd81e0f5b775967b3ed5f1a058d9dceb7
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-04-13 22:23:48 +0200

    Initial backend project structure

---
+++ dist/scripts/game.js
@@ -0,0 +1,5 @@
+"use strict";
+const hello = document.createElement("h1");
+hello.textContent = "Hello, World!";
+document.body.appendChild(hello);
+//# sourceMappingURL=game.js.map
---
+++ src/scripts/game.ts
@@ -0,0 +1,3 @@
+const hello: HTMLElement = document.createElement("h1")
+hello.textContent = "Hello, World!"
+document.body.appendChild(hello)
