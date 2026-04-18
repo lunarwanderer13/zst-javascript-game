3406828370ccc6af0caf936de812c000bcaa044b
lunarwanderer13 <lunarwanderer13@gmail.com>
2026-04-18 11:21:16 +0200

    Add comments to player.ts

--- src/scripts/player.ts
+++ src/scripts/player.ts
@@ -1,22 +1,26 @@
+// Player class controlling the player
 export default class Player {
-    public container: HTMLDivElement
-    public element: HTMLImageElement
+    public container: HTMLDivElement // The game container <div> element
+    public element: HTMLImageElement // The player <img> element
 
-    public alive: boolean
-    public y: number
-    public velocity: number
-    private jump_force: number
-    private sprite: string
+    public alive: boolean      // Whether the player is alive or not
+    public y: number           // The player's current vertical position
+    public velocity: number    // The player's current vertical velocity
+    private jump_force: number // The force of player's jump
+    private sprite: string     // Player's sprite
 
     public constructor(container: HTMLDivElement) {
         this.container = container
 
-        this.alive = true
-        this.y = 50.0
-        this.velocity = 0.0
+        this.alive = true   // Alive by default
+        this.y = 50.0       // Start falling from 50% height
+        this.velocity = 0.0 // Start with no velocity
+
         this.jump_force = 1.5
         this.sprite = "./src/images/Placeholder.png"
 
+        // Create the player <img> element
+        // and append it to the game container <div> element
         this.element = new Image()
         this.element.src = this.sprite
         this.element.alt = "Cat"
@@ -24,15 +28,17 @@ export default class Player {
         this.container.appendChild(this.element)
     }
 
+    // Player jump handler
     public jump(): void {
-        this.velocity = -this.jump_force
+        this.velocity = -this.jump_force // Add negative force to go upward
     }
 
+    // Gravity handler
     private gravity(): void {
-        let v: number = this.velocity
-        this.velocity = Math.min(v + 0.075, 1.5)
+        this.velocity = Math.min(this.velocity + 0.075, 1.5) // Gradually increase the velocity, max 1.5
     }
 
+    // Player movement handler
     public move(): void {
         if (!this.alive) return
 
@@ -42,6 +48,7 @@ export default class Player {
         this.element.style.transform = `rotate(${this.velocity * 10}deg)`
     }
 
+    // Game over animation
     public die(): void {
         this.alive = false
 
@@ -52,9 +59,9 @@ export default class Player {
                 { offset: 1.0, left: "-5%", top: `${this.y + 135.0}%`, rotate: `-90deg` }            // Fall down
             ],
             {
-                duration: 1500,
-                easing: "linear",
-                fill: "forwards"
+                duration: 1500,   // 1.5s duration
+                easing: "linear", // no easing function
+                fill: "forwards"  // keep changes after animtion finishes
             }
         )
     }
