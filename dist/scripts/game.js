bbf885be057448e8bf1612cdd745270d404ea625
LunarWanderer月 <76808845+lunarwanderer13@users.noreply.github.com>
2026-04-18 11:32:20 +0200

    Add comments to all .ts and .js files (#23)
    * Add comments to player.ts
    
    * Add comments to game.ts
    
    * Recompile with comments

--- dist/scripts/game.js
+++ dist/scripts/game.js
@@ -1,14 +1,21 @@
+// Check if it's loading properly
 console.log("Hello, World!");
 import Player from "./player.js";
+// Game handler
 function main() {
+    // The game container <div> element
     const game_container = document.querySelector(".game-container");
     if (!game_container)
         return;
+    // Initialize a player
     const player = new Player(game_container);
+    // Pause game on load, giving the player time to read the rules and lock in
     let game_running = false;
+    // Button used for jumping, and if the game is paused, starting the game
     const jump_button = document.querySelector("button#jump-button");
     if (!jump_button)
         return;
+    // Jump handler
     function trigger_jump() {
         if (!game_running) {
             game_running = true;
@@ -17,6 +24,7 @@ function main() {
         }
         player.jump();
     }
+    // Listeners for user input
     jump_button.addEventListener("click", trigger_jump); // Button click
     document.addEventListener("keydown", (event) => {
         if (event.code === "Space") {
@@ -24,13 +32,14 @@ function main() {
             trigger_jump();
         }
     });
+    // Main game loop
     const game_loop = setInterval(() => {
         if (game_running) {
             player.move();
             if (player.y < 0 || player.y > 100) {
                 player.die();
                 console.log("game over");
-                clearInterval(game_loop);
+                clearInterval(game_loop); // Stop the game loop
             }
         }
     }, 10);
--- src/scripts/game.ts
+++ src/scripts/game.ts
@@ -1,18 +1,25 @@
+// Check if it's loading properly
 console.log("Hello, World!")
 
 import Player from "./player.js"
 
+// Game handler
 function main(): void {
+    // The game container <div> element
     const game_container: HTMLDivElement | null = document.querySelector<HTMLDivElement>(".game-container")
     if (!game_container) return
 
+    // Initialize a player
     const player: Player = new Player(game_container)
 
+    // Pause game on load, giving the player time to read the rules and lock in
     let game_running: boolean = false
 
+    // Button used for jumping, and if the game is paused, starting the game
     const jump_button: HTMLButtonElement | null = document.querySelector<HTMLButtonElement>("button#jump-button")
     if (!jump_button) return
 
+    // Jump handler
     function trigger_jump(): void {
         if (!game_running) {
             game_running = true
@@ -22,6 +29,7 @@ function main(): void {
         player.jump()
     }
 
+    // Listeners for user input
     jump_button.addEventListener("click", trigger_jump)              // Button click
     document.addEventListener("keydown", (event: KeyboardEvent) => { // Space press
         if (event.code === "Space") {
@@ -30,6 +38,7 @@ function main(): void {
         }
     })
 
+    // Main game loop
     const game_loop: number = setInterval(() => {
         if (game_running) {
             player.move()
@@ -37,7 +46,7 @@ function main(): void {
             if (player.y < 0 || player.y > 100) {
                 player.die()
                 console.log("game over")
-                clearInterval(game_loop)
+                clearInterval(game_loop) // Stop the game loop
             }
         }
     }, 10)
